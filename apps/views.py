c46e07aa112cbcbaf0aec107f09f58ed2a7daf78
yangchuanhu <ych_1024@163.com>
2018-04-26 12:36:48 +0800

    首页前端样式

--- apps/views.py
+++ apps/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 
 from apps.product.models import Product
 
+from utils.time_utils import date
+
 
 def index_view(request):
-    products = Product.objects.filter(public=True).order_by('-vote_count', '-created_at')
+    products_dict = dict()
+    for i in range(3):
+        _date = date(-i).date()
+        products_dict[_date.strftime('%Y-%m-%d')] = Product.objects.filter(
+            public=True, created_at__contains=_date).\
+            order_by('-vote_count', '-created_at')
+    # products = Product.objects.filter(public=True).order_by('-vote_count', '-created_at')
     context = {
-        'products': products
+        'products_dict': products_dict
     }
     return render(request, 'index.html', context)
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
-__author__ = 'ych'
-__date__ = '17/04/2018 21:12'
+from datetime import  datetime, timedelta
+
+
+def date(delta=-1):
+    return datetime.today() + timedelta(delta)
