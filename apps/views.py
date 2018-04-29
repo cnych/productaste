590b0210d525e35ed0d0684ef9f07c9b9454d8d6
yangchuanhu <ych_1024@163.com>
2018-04-29 18:33:32 +0800

    原生JS完成加载更多的功能

--- apps/views.py
+++ apps/views.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 from django.shortcuts import render
 
 from apps.product.models import Product
 
-from utils.time_utils import date
+from utils.time_utils import date, str2date
 
 
 def index_view(request):
-    products_dict = dict()
-    for i in range(3):
-        _date = date(-i).date()
-        products_dict[_date.strftime('%Y-%m-%d')] = Product.objects.filter(
-            public=True, created_at__contains=_date).\
-            order_by('-vote_count', '-created_at')
-    # products = Product.objects.filter(public=True).order_by('-vote_count', '-created_at')
-    context = {
-        'products_dict': products_dict
-    }
-    return render(request, 'index.html', context)
+    last_dt = request.GET.get('last_dt', None)
+    if last_dt is None:  # 默认的首页
+        products_dict = dict()
+        for i in range(3):
+            _date = date(-i).date()
+            _key = _date.strftime('%Y-%m-%d')
+            products_dict[_key] = Product.objects.filter(
+                public=True, created_at__contains=_key).\
+                order_by('-vote_count', '-created_at')
+        context = {
+            'products_dict': products_dict
+        }
+        return render(request, 'index.html', context)
+    else:  # 渲染前一天的数据
+        _date = str2date(last_dt) + timedelta(-1)
+        _key = _date.strftime('%Y-%m-%d')
+        context = {
+            'date': _key,
+            'products': Product.objects.filter(public=True, created_at__contains=_key).
+                order_by('-vote_count', '-created_at')
+        }
+        return render(request, 'product-item.html', context)
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -4,3 +4,7 @@ from datetime import  datetime, timedelta
 
 def date(delta=-1):
     return datetime.today() + timedelta(delta)
+
+
+def str2date(dt, ft='%Y-%m-%d'):
+    return datetime.strptime(dt, ft).date()
